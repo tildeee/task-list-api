362df9b4ee004c8f71fa613c211fdef89f17e3a8
simon <simon@adadevelopersacademy.org>
2021-04-15 22:02:47 -0700

    more personal impl

---
+++ app/models/task.py
@@ -0,0 +1,12 @@
+from flask import current_app
+from app import db
+from datetime import datetime
+
+
+class Task(db.Model):
+    task_id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String)
+    description = db.Column(db.String)
+    completed_at = db.Column(db.DateTime, nullable=True)
+    goal_id = db.Column(db.Integer, db.ForeignKey(
+        'goal.goal_id'), nullable=True)
